53fbd02aa3de37eb45e659b750514c0e46f1a7ed
Petr Kmoch <petr.kmoch@gmail.com>
2023-03-04 16:51:10 +0100

    Call yt-dlp on Run
    First working version

--- code/download.py
+++ code/download.py
@@ -1,4 +1,9 @@
+# Copyright 2023 Petr Kmoch
+# This file is licensed under the MIT License; please see accompanying
+# LICENSE file or https://opensource.org/license/mit/
+
 import os.path
+import shutil
 import sys
 import tkinter as tk
 import tkinter.filedialog
@@ -9,6 +14,41 @@ import yt_dlp as yt
 
 
 
+class SilentLogger():
+    def debug(*args): pass
+    def info(*args): pass
+    def warning(*args): pass
+    def error(*args): pass
+    def critical(*args): pass
+
+
+
+class Runtime():
+    progress_download_unknown = 25
+    progress_postprocess = 80
+    progress_copy = 90
+    
+    def __init__(self):
+        self._is_setup = False
+        self.max = None
+
+    def setup(self, max_exact, max_approx):
+        if self._is_setup:
+            return
+        self._is_setup = True
+        if max_exact is not None:
+            self.max = max_exact
+        elif max_approx is not None:
+            self.max = max_approx
+
+    def set_progress(self, progressor, value):
+        if self.max is not None:
+            progressor.set(min(100 * value / self.max, self.progress_postprocess))
+        else:
+            progressor.set(self.progress_download_unknown)
+
+
+
 class MainWindow(tk.Tk):
     default_format_extensions = {
         "audio": "mp3",
@@ -16,6 +56,8 @@ class MainWindow(tk.Tk):
     }
 
     def __init__(self, *args, **kwargs):
+        self.ffmpeg_location = kwargs["ffmpeg_location"]
+        del kwargs["ffmpeg_location"]
         super().__init__(*args, **kwargs)
 
         sticky_all = (tk.N, tk.S, tk.W, tk.E)
@@ -47,7 +89,7 @@ class MainWindow(tk.Tk):
         frm_input.grid(column=0, row=0, sticky="nwe", padx=pad, pady=pad)
 
         ttk.Label(frm_input, text="URL:", anchor=tk.E).grid(column=0, row=0, sticky=tk.E, padx=(pad, 0), pady=pad)
-        self.ent_url = ttk.Entry(frm_input, textvariable=self.url)
+        self.ent_url = ttk.Entry(frm_input, textvariable=self.url, width=30)
         self.ent_url.grid(column=1, row=0, sticky=sticky_all, pady=pad)
 
         self.btn_paste = ttk.Button(frm_input, text="Paste", command=self.paste_url)
@@ -59,7 +101,7 @@ class MainWindow(tk.Tk):
         frm_output.grid(column=0, row=1, sticky="nwe", padx=pad, pady=pad)
 
         ttk.Label(frm_output, text="File:", anchor=tk.E).grid(column=0, row=0, sticky=tk.E, padx=(pad, 0), pady=pad)
-        self.ent_file = ttk.Entry(frm_output, textvariable=self.file)
+        self.ent_file = ttk.Entry(frm_output, textvariable=self.file, width=75)
         self.ent_file.grid(column=1, row=0, sticky=sticky_all, pady=pad)
 
         self.btn_browse = ttk.Button(frm_output, text="Browse", command=self.browse_file)
@@ -93,6 +135,21 @@ class MainWindow(tk.Tk):
         self.state.set("")
         self.progress.set(0)
 
+    def set_state_downloading(self):
+        self.state.set("Downloading...")
+
+    def set_state_postprocessing(self):
+        self.progress.set(Runtime.progress_postprocess)
+        self.state.set("Converting...")
+
+    def set_state_copying(self):
+        self.progress.set(Runtime.progress_copy)
+        self.state.set("Copying...")
+
+    def set_state_done(self):
+        self.progress.set(100)
+        self.state.set("Done!")
+
     def config_changed(self, *args):
         self.set_state_idle()
         if self.url.get() and self.file.get():
@@ -120,29 +177,49 @@ class MainWindow(tk.Tk):
             return
         self.file.set(file)
 
+    def on_download_progress(self, prg):
+        status = prg["status"]
+        if status == "error":
+            raise RuntimeError("Something went wrong")
+        elif status == "finished":
+            self.set_state_postprocessing()
+        elif status == "downloading":
+            self.runtime.setup(prg.get("total_bytes"), prg.get("total_bytes_estimate"))
+            self.runtime.set_progress(self.progress, prg.get("downloaded_bytes"))
+        self.update()
+
+    def on_postprocessing_progress(self, prg):
+        self.update()
+
     def run(self):
         ydl_opts = {
+            "ffmpeg_location": self.ffmpeg_location,
+            "format": self.default_format_extensions["video"],
             "keepvideo": True,
+            "logging": SilentLogger(),
+            "outtmpl": "%(id)s.%(ext)s",
+            "postprocessor_hooks": [self.on_postprocessing_progress],
             "progress_hooks": [self.on_download_progress],
-            "postprocessor_hooks": [self.on_postprocessing_progress]
+            "quiet": True,
         }
         if self.format.get() == "audio":
-            postprocessor = {
+            ydl_opts["postprocessors"] = [{
                 "key": "FFmpegExtractAudio",
                 "preferredcodec": self.default_format_extensions[self.format.get()]
-            }
-        else:
-            postprocessor = {
-                "key": "FFmpegExtractAudio",
-                "preferredcodec": self.default_format_extensions[self.format.get()]
-            }
-        ydl_opts["postprocessors"] = [postprocessor]
-        # ToDo continue here to actually run yt_dlp
+            }]
+        self.runtime = Runtime()
+        self.set_state_downloading()
+        with yt.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(self.url.get())
+        self.set_state_copying()
+        downloaded = f"{info['id']}.{self.default_format_extensions[self.format.get()]}"
+        shutil.copy2(downloaded, self.file.get())
+        self.set_state_done()
 
 
 
 def main(args):
-    main_window = MainWindow()
+    main_window = MainWindow(ffmpeg_location=args[1])
     main_window.mainloop()
 
 
